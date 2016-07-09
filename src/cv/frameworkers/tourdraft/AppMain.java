a1e0819cabf7070ad9f43bb0ed417390b4812d53
Anaximandro Andrade <anaxfive@hotmail.com>
2016-07-09 00:30:52 -0100

    Feito desenho base da tela inicial - com imagem no fundo

---
+++ src/cv/frameworkers/tourdraft/AppMain.java
@@ -0,0 +1,37 @@
+package cv.frameworkers.tourdraft;/**
+ * Created by AnaxAndrade on 08-07-2016.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("view/TelaInicial.fxml"));
+            BorderPane pane = (BorderPane) loader.load();
+
+            Scene scene = new Scene(pane);
+            primaryStage.resizableProperty().setValue(false);
+            primaryStage.setTitle("Taekwondo Draft");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
